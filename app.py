c996ecbe9bc45539b360b3afb1e106e4d70a37f1
P J Anthony <104015990+pjanthony2001@users.noreply.github.com>
2025-04-07 00:33:17 +0200

    Fix routing errors
    Some of the routes were incorrectly spelled.

--- app.py
+++ app.py
@@ -24,48 +24,49 @@ def alive():
     return make_response(jsonify({"message" : "Alive"}), 200)
 
 
-@app.route("/api/association/", methods=["GET"])
+@app.route("/api/associations/", methods=["GET"])
 def list_asso():
-    return make_response(associations_df.to_json(orient="records", force_ascii=False), 200) 
+    return make_response(associations_df.to_json(orient="records", force_ascii=True), 200) 
 
-@app.route("/api/eventments/", methods=["GET"])
+@app.route("/api/evenements/", methods=["GET"])
 def list_event():
     return make_response(evenements_df.to_json(orient="records", force_ascii=False), 200) 
 
 @app.route("/api/association/<int:id>", methods=["GET"])
 def find_asso(id: int):
-    try: 
+    if id in associations_df['id'].values:
         student_info = associations_df[associations_df.id == id]
-    except IndexError:
-        return make_response(f"Student details for {id} not found!", 404) 
-    return make_response(student_info.to_json(orient="records", force_ascii=False), 200) 
+        return make_response(student_info.to_json(orient="records", force_ascii=False), 200) 
 
+    return make_response(f"Student details for {id} not found!", 404) 
 
-@app.route("/api/evenements/<int:id>", methods=["GET"])
+@app.route("/api/evenement/<int:id>", methods=["GET"])
 def find_event(id: int):
-    try: 
+    if id in evenements_df['id'].values: 
         event_info = evenements_df[evenements_df.id == id]
-    except IndexError:
-        return make_response(f"Event details for {id} not found!", 404) 
-    return make_response(event_info.to_json(orient="records", force_ascii=False), 200) 
+        return make_response(event_info.to_json(orient="records", force_ascii=False), 200) 
+    
+    return make_response(f"Event details for {id} not found!", 404) 
 
 
-@app.route("/api/association/<int:id>/eventments", methods=["GET"])
+@app.route("/api/association/<int:id>/evenements", methods=["GET"])
 def find_asso_event(id: int):
-    try: 
+    if id in evenements_df['association_id'].values:
         student_info = evenements_df[evenements_df.association_id == id]
-    except IndexError:
-        return make_response(f"Student details for {id} not found!", 404) 
-    return make_response(student_info.to_json(orient="records", force_ascii=False), 200) 
+        return make_response(student_info.to_json(orient="records", force_ascii=False), 200) 
+    
+    return make_response(f"Student details for {id} not found!", 404) 
+    
     
 @app.route("/api/association/type/<type>", methods=["GET"])
-def find_asso_type(type: str):
-    try: 
-        student_info = associations_df[associations_df.type == type]
-    except IndexError:
-        return make_response(f"Student details for {type} not found!", 404) 
+def find_asso_type(type_: str):
+    if type_ in associations_df['type'].values:
+        student_info = associations_df[associations_df.type == type_]
+        return make_response(student_info.to_json(orient="records", force_ascii=False), 200) 
 
-    return make_response(student_info.to_json(orient="records", force_ascii=False), 200) 
+
+    return make_response(f"Student details for {type_} not found!", 404) 
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
